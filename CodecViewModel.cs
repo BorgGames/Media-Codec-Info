c1d646282c15b12d47eed9f703a9586a047b3f98
Victor Nova <lostfreeman@gmail.com>
2023-03-01 11:29:59 -0800

    initial version

---
+++ CodecViewModel.cs
@@ -0,0 +1,42 @@
+﻿namespace UWP_Codec_Info {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+
+    using Windows.Media.Core;
+
+    public class CodecViewModel {
+        public CodecKind Kind { get; set; }
+        public string DisplayName { get; set; }
+        public CodecCategory Category { get; set; }
+        public string Features { get; set; }
+
+        public CodecViewModel(CodecInfo codecInfo) {
+            this.Kind = codecInfo.Kind;
+            this.DisplayName = codecInfo.DisplayName;
+            this.Category = codecInfo.Category;
+            this.Features = string.Join(Environment.NewLine, codecInfo.Subtypes.Select(SubtypeToFeature));
+        }
+
+        static string SubtypeToFeature(string subtype)
+            => subtype2name.TryGetValue(subtype, out string feature) ? feature : subtype;
+
+        static readonly Dictionary<string, string> subtype2name = new Dictionary<string, string>() { };
+
+        static CodecViewModel() {
+            var knownSubtypes = typeof(CodecSubtypes).GetProperties(BindingFlags.Public | BindingFlags.Static);
+            foreach (var knownSubtype in knownSubtypes) {
+                string featureName = knownSubtype.Name;
+                if (featureName.StartsWith("VideoFormat"))
+                    featureName = featureName.Substring("VideoFormat".Length).ToUpperInvariant();
+                if (featureName.StartsWith("AudioFormat"))
+                    featureName = featureName.Substring("AudioFormat".Length).ToUpperInvariant();
+                string subtype = (string)knownSubtype.GetValue(null);
+                subtype2name.Add(subtype, featureName);
+            }
+        }
+
+        public CodecViewModel() { }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace UWP_Codec_Info {
+    using System;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Windows.Media.Core;
+    using Windows.UI.Xaml.Controls;
+
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        async Task EnumerateCodecs() {
+            var codecQuery = new CodecQuery();
+            var videoEncoders = await codecQuery.FindAllAsync(CodecKind.Video, CodecCategory.Encoder, "");
+            var audioEncoders = await codecQuery.FindAllAsync(CodecKind.Audio, CodecCategory.Encoder, "");
+            var videoDecoders = await codecQuery.FindAllAsync(CodecKind.Video, CodecCategory.Decoder, "");
+            var audioDecoders = await codecQuery.FindAllAsync(CodecKind.Audio, CodecCategory.Decoder, "");
+            var allCodecs = videoEncoders.Concat(audioEncoders).Concat(videoDecoders).Concat(audioDecoders);
+            this.DataContext = new ObservableCollection<CodecViewModel>(allCodecs.Select(c => new CodecViewModel(c)));
+        }
+
+        async void Page_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e) {
+            await this.EnumerateCodecs();
+        }
+    }
+}
